74e7d8e9da4d1a3fdb07b139a471ccd50f0826bc
German <102919783+germza19@users.noreply.github.com>
2024-01-28 17:17:07 -0300

    animations

--- Assets/Ger-Scripts/Controllers/MainMenuController.cs
+++ Assets/Ger-Scripts/Controllers/MainMenuController.cs
@@ -5,8 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject credits;
+
+    private void Start()
+    {
+
+        menu.SetActive(true);
+        credits.SetActive(false);  
+    }
+    public void OnCreditsButton()
+    {
+        AudioManager.Instance.PlayOneShot(FmodEvents.Instance.UIClick, transform.position);
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+    public void OnBackCreditsButton()
+    {
+        AudioManager.Instance.PlayOneShot(FmodEvents.Instance.UIClick, transform.position);
+        menu.SetActive(true);
+        credits.SetActive(false);
+    }
+    public void OnExitButton()
+    {
+        AudioManager.Instance.PlayOneShot(FmodEvents.Instance.UIClick, transform.position);
+        Application.Quit();
+    }
     public void OnPlayButton()
     {
+        AudioManager.Instance.PlayOneShot(FmodEvents.Instance.UIClick, transform.position);
         SceneManager.LoadScene(1);
     }
 }
